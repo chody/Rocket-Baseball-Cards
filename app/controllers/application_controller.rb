40a4de7a60668886cf7952363b971119115018a1
Cody Hill <cody.hill07@gmail.com>
2019-02-20 00:36:00 -0600

    properly pass tests with fixtures

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,13 +5,9 @@ class ApplicationController < ActionController::Base
 	before_action :authenticate_user!
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :ebay_auth, unless: :test_env?
+  before_action :ebay_auth
 
   	protected
-
-    def test_env?
-      Rails.env == 'test'
-    end
 
   	def configure_permitted_parameters
     	devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name address city state zip phone])
